e3c8b4bd287cf80dc2503a1f5f6c1a9b2ed5ee7a
smart-life-tech <christlightworld@gmail.com>
2024-05-15 21:22:09 +0100

    modified:   withInt/withInt.ino

--- withInt/withInt.ino
+++ withInt/withInt.ino
@@ -1,6 +1,4 @@
-const int sensorPin = 4;
-const int ledPin = 2;            // Define the pin for the LED
-const int buzzerPin = 3;         // Define the pin for the buzzer
+const int sensorPin = 2;
 volatile int vibrationCount = 0; // Variable to count vibrations
 
 // Interrupt service routine for counting vibrations
@@ -13,8 +11,6 @@ void setup()
 {
     Serial.begin(9600);                                                // Start serial communication at 9600 baud rate
     pinMode(sensorPin, INPUT);                                         // Set the sensorPin as an input pin
-    pinMode(ledPin, OUTPUT);                                           // Set the ledPin as an output pin
-    pinMode(buzzerPin, OUTPUT);                                        // Set the buzzerPin as an output pin
     attachInterrupt(digitalPinToInterrupt(sensorPin), count, FALLING); // Attach interrupt to sensorPin
 }
 
@@ -30,28 +26,20 @@ void loop()
         // Do nothing, just wait for 1 second while the ISR counts vibrations
     }
 
-    // Calculate the frequency in Hz
-    float frequency = vibrationCount / 1.0;
-
-    // Print the frequency to the Serial Monitor
-    Serial.print("Frequency: ");
-    Serial.print(frequency);
-    Serial.println(" Hz");
-
     // Indicate vibration with LED and buzzer if frequency is detected
     if (vibrationCount > 0)
-    {
+    { // Calculate the frequency in Hz
+        float frequency = vibrationCount / 1.0;
+
+        // Print the frequency to the Serial Monitor
+        Serial.print("Frequency: ");
+        Serial.print(frequency);
+        Serial.println(" Hz");
+
         Serial.println("Detected vibration...");
-        digitalWrite(ledPin, HIGH); // Turn on the LED
-        tone(buzzerPin, 1000);      // Play a 1kHz tone on the buzzer
-    }
-    else
-    {
-        Serial.println("...");
-        digitalWrite(ledPin, LOW); // Turn off the LED
-        noTone(buzzerPin);         // Stop playing any tone on the buzzer
+        vibrationCount=0;
     }
 
     // Add a delay to avoid flooding the serial monitor
-    delay(1000);
+    delay(10);
 }
